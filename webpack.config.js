a8aba1443a59c2638c5a4d43e2d287dad0844752
Blain Crawford <blaincrawford@gmail.com>
2022-04-26 16:19:37 -0400

    fixed babel/core and have pdf renderer in

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
+  resolve: {
+    fallback: {
+      process: require.resolve("process/browser"),
+      zlib: require.resolve("browserify-zlib"),
+      stream: require.resolve("stream-browserify"),
+      util: require.resolve("util"),
+      buffer: require.resolve("buffer"),
+      asset: require.resolve("assert"),
+    }
+  },
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, './dist'),
@@ -31,6 +42,10 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+      process: "process/browser",
+    }),
     new HtmlWebpackPlugin({
       template: './src/index.html'
     })
